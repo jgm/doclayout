d8618a05891ed55ab49765cd68fa86ff6b2d87f7
John MacFarlane <jgm@berkeley.edu>
2019-07-31 10:02:45 -0700

    More benchmarks.

--- bench/bench.hs
+++ bench/bench.hs
@@ -13,13 +13,21 @@ bigtext = "Hello there. This is a big text."
 
 cases :: [Benchmark]
 cases =
-  [ bench "sample document" $
+  [ bench "sample document 1" $
       nf (render Nothing)
          (nest 3 $ prefixed "> " $ vcat $ replicate 15 $
            hsep $ map text $ words bigtext)
-  , bench "sample document (Text.Pandoc.Pretty)" $
+  , bench "sample document 1 (Text.Pandoc.Pretty)" $
       nf (P.render Nothing :: P.Doc -> Text)
          (P.nest 3 $ P.prefixed "> " $ P.vcat $ replicate 15 $
+           P.hsep $ map P.text $ words bigtext)
+  , bench "sample document 2" $
+      nf (render Nothing)
+         (nest 3 $ cblock 20 $ vcat $ replicate 15 $
+           hsep $ map text $ words bigtext)
+  , bench "sample document 2 (Text.Pandoc.Pretty)" $
+      nf (P.render Nothing :: P.Doc -> Text)
+         (P.nest 3 $ P.cblock 20 $ P.vcat $ replicate 15 $
            P.hsep $ map P.text $ words bigtext)
   , bench "soft spaces at end of line" $
       nf (render Nothing)
