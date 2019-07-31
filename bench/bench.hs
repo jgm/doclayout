2fb683f0121709b0cc7c59b10303828ce7b80b44
John MacFarlane <jgm@berkeley.edu>
2019-07-31 09:57:38 -0700

    Benchmark old Pandoc module too.

--- bench/bench.hs
+++ bench/bench.hs
@@ -1,13 +1,27 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Text.DocLayout
+import Data.Text (Text)
 import Criterion.Main
 import Criterion.Types (Config (..))
+import qualified Text.Pandoc.Pretty as P
 
 main :: IO ()
 main = defaultMainWith defaultConfig{ timeLimit = 10.0 } $ cases
 
+bigtext :: String
+bigtext = "Hello there. This is a big text."
+
 cases :: [Benchmark]
-cases = [
-  bench "soft spaces at end of line" $
-    nf (render Nothing) ("a" <> mconcat (replicate 50 (space <> box 1 mempty)))
-    ]
+cases =
+  [ bench "sample document" $
+      nf (render Nothing)
+         (nest 3 $ prefixed "> " $ vcat $ replicate 15 $
+           hsep $ map text $ words bigtext)
+  , bench "sample document (Text.Pandoc.Pretty)" $
+      nf (P.render Nothing :: P.Doc -> Text)
+         (P.nest 3 $ P.prefixed "> " $ P.vcat $ replicate 15 $
+           P.hsep $ map P.text $ words bigtext)
+  , bench "soft spaces at end of line" $
+      nf (render Nothing)
+         ("a" <> mconcat (replicate 50 (space <> box 1 mempty)))
+  ]
