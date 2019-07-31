1d57eab5fc182a9313e131197a626edfce8f5644
John MacFarlane <jgm@berkeley.edu>
2019-07-30 17:14:05 -0700

    Fix compiler warnings.

--- test/test.hs
+++ test/test.hs
@@ -1,15 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-import Text.DocLayout
-import Test.Tasty.Golden
+-- import Text.DocLayout
+-- import Test.Tasty.Golden
 import Test.Tasty
-import Test.Tasty.HUnit
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
-import qualified Data.Text.Encoding as T
-import System.FilePath
-import Data.Semigroup ((<>))
+-- import Test.Tasty.HUnit
+-- import qualified Data.Text as T
+-- import qualified Data.Text.IO as T
+-- import qualified Data.Text.Encoding as T
+-- import System.FilePath
+-- import Data.Semigroup ((<>))
 
 main :: IO ()
 main = defaultMain $ testGroup "Tests"
