9e8dbc56eccc977258b48c1c7d32c569d37d3ec9
John MacFarlane <jgm@berkeley.edu>
2019-08-05 12:06:14 -0700

    Revert "Add newline at end of every document."
    This reverts commit f1a7627c021553ac2da4ed23fe3b739b173b5900.

--- test/test.hs
+++ test/test.hs
@@ -13,27 +13,27 @@ tests :: [TestTree]
 tests =
   [ testCase "simple vcat " $
       render (Just 10) (vcat $ map chomp ["aaa", "bbb", "ccc"])
-      @?= ("aaa\nbbb\nccc\n" :: String)
+      @?= ("aaa\nbbb\nccc" :: String)
 
   , testCase "variant" $
       render (Just 10) ((chomp "aaa") $$ (chomp "bbb") $$ (chomp "ccc"))
-      @?= ("aaa\nbbb\nccc\n" :: String)
+      @?= ("aaa\nbbb\nccc" :: String)
 
   , testCase "simple text above line length" $
       render (Just 4) ("hello" <+> "there")
-      @?= ("hello\nthere\n" :: String)
+      @?= ("hello\nthere" :: String)
 
   , testCase "cr" $
       render (Just 60) ("hello" <> cr <> "there")
-      @?= ("hello\nthere\n" :: String)
+      @?= ("hello\nthere" :: String)
 
   , testCase "wrapping" $
       render (Just 10) (hsep ["hello", "there", "this", "is", "a", "test"])
-      @?= ("hello\nthere this\nis a test\n" :: String)
+      @?= ("hello\nthere this\nis a test" :: String)
 
   , testCase "simple box wrapping" $
       render (Just 50) (box 3 "aa" <> box 3 "bb" <> box 3 ("aa" <+> "bbbb"))
-      @?= ("aa bb aa\n      bbbb\n" :: Text)
+      @?= ("aa bb aa\n      bbbb" :: Text)
 
   , testCase "nontrivial empty doc" $
       isEmpty (nest 5 (alignCenter empty))
@@ -45,74 +45,74 @@ tests =
 
   , testCase "prefixed with multi paragraphs" $
       render (Just 80) (prefixed "> " ("foo" <> cr <> "bar" <> blankline <> "baz"))
-      @?= ("> foo\n> bar\n>\n> baz\n" :: String)
+      @?= ("> foo\n> bar\n>\n> baz" :: String)
 
   , testCase "breaking space before empty box" $
       render Nothing ("a" <> space <> box 3 mempty)
-      @?= ("a\n" :: String)
+      @?= ("a" :: String)
 
   , testCase "centered" $
       render (Just 10) (alignCenter "hi\nlow")
-      @?= ("    hi\n   low\n" :: String)
+      @?= ("    hi\n   low" :: String)
 
   , testCase "vfill" $
       render Nothing (vfill "|" <> box 2 (vcat $ replicate 4 "aa") <>
                       vfill "|")
-      @?= ("|aa|\n|aa|\n|aa|\n|aa|\n" :: Text)
+      @?= ("|aa|\n|aa|\n|aa|\n|aa|" :: Text)
 
   , testCase "nest" $
       render Nothing (nest 4 "aa\n\nbb\ncc")
-      @?= ("    aa\n\n    bb\n    cc\n" :: Text)
+      @?= ("    aa\n\n    bb\n    cc" :: Text)
 
   , testCase "aligned" $
       render Nothing ("aa" <> aligned ("bb" $$ "cc") <> "dd")
-      @?= ("aabb\n  ccdd\n" :: Text)
+      @?= ("aabb\n  ccdd" :: Text)
 
   , testCase "align with box" $
       render Nothing ("aa" <> box 2 ("bb" $$ "cc") <> "dd")
-      @?= ("aabbdd\n  cc\n" :: Text)
+      @?= ("aabbdd\n  cc" :: Text)
 
   , testCase "centered box" $
       render Nothing ("aa" <> box 4 (alignCenter $ "bb" $$ "cc") <> "dd")
-      @?= ("aa bb dd\n   cc\n" :: Text)
+      @?= ("aa bb dd\n   cc" :: Text)
 
   , testCase "blanks at beginning" $
       render Nothing (blanklines 2 <> "aa")
-      @?= ("aa\n" :: Text)
+      @?= ("aa" :: Text)
 
   , testCase "blanks at end" $
       render Nothing ("aa" <> blanklines 2)
-      @?= ("aa\n" :: Text)
+      @?= ("aa" :: Text)
 
   , testCase "chomp 1" $
       render Nothing (chomp (("aa" <> space) <> blankline) <> "bb")
-      @?= ("aabb\n" :: Text)
+      @?= ("aabb" :: Text)
 
   , testCase "chomp 2" $
       render Nothing (chomp ("aa" <> space) <> "bb")
-      @?= ("aabb\n" :: Text)
+      @?= ("aabb" :: Text)
 
   , testCase "chomp 3" $
       render Nothing (chomp "aa")
-      @?= ("aa\n" :: Text)
+      @?= ("aa" :: Text)
 
   , testCase "chomp with nesting" $
       render Nothing (chomp (nest 3 ("aa" <> blankline)) <> "bb" <> cr <> "cc")
-      @?= ("   aabb\ncc\n" :: Text)
+      @?= ("   aabb\ncc" :: Text)
 
   , testCase "chomp with alignment" $
       render (Just 4) (chomp (alignCenter ("aa\nbb" <> blankline)))
-      @?= (" aa\nbb\n" :: Text)
+      @?= (" aa\nbb" :: Text)
       -- last line is left aligned because we pop alignment before
       -- the line is emitted
 
   , testCase "chomp with box at end" $
       render Nothing ("aa" <> cr <> chomp (box 2 ("aa" <> blankline) <> blankline))
-      @?= ("aa\naa\n" :: Text)
+      @?= ("aa\naa" :: Text)
 
   , testCase "empty and $$" $
       render Nothing ("aa" $$ empty $$ "bb")
-      @?= ("aa\nbb\n" :: Text)
+      @?= ("aa\nbb" :: Text)
 
   , testCase "table" $
       render Nothing ((rblock 4 "aa" <> lblock 3 " | " <> cblock 4 "bb" <>
@@ -121,6 +121,6 @@ tests =
                           lblock 3 " | " <> lblock 4 "----") $$
                       (rblock 4 "dd" <> lblock 3 " | " <> cblock 4 "ee" <>
                           lblock 3 " | " <> lblock 4 "ff"))
-      @?= ("  aa |  bb  | cc\n---- | ---- | ----\n  dd |  ee  | ff\n" :: Text)
+      @?= ("  aa |  bb  | cc\n---- | ---- | ----\n  dd |  ee  | ff" :: Text)
 
   ]
