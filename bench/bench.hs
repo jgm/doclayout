f3c591ff5c62e97110848f8534e346c2b84f2dfa
John MacFarlane <jgm@berkeley.edu>
2019-08-09 20:42:35 -0700

    Adjust benchmark.

--- bench/bench.hs
+++ bench/bench.hs
@@ -11,30 +11,33 @@ main = defaultMainWith defaultConfig{ timeLimit = 10.0 } $ cases
 bigtext :: String
 bigtext = "Hello there. This is a big text."
 
+flowedDoc :: Doc
+flowedDoc = hsep $ map text $ words . unwords $ replicate 500 bigtext
+
 cases :: [Benchmark]
 cases =
-  [ bench "sample document 1" $
-      nf (render Nothing :: Doc -> LazyText)
-         (nest 3 $ prefixed "> " $ vcat $ replicate 15 $
-           hsep $ map text $ words bigtext)
+  [
+--    bench "sample document 1" $
+--      nf (render Nothing :: Doc -> LazyText)
+--         (nest 3 $ prefixed "> " $ vcat $ replicate 15 $
+--           hsep $ map text $ words bigtext)
+--
+--  , bench "sample document 2" $
+--      nf (render Nothing :: Doc -> LazyText)
+--         (nest 3 $ cblock 20 $ vcat $ replicate 15 $
+--           hsep $ map text $ words bigtext)
 
-  , bench "sample document 2" $
-      nf (render Nothing :: Doc -> LazyText)
-         (nest 3 $ cblock 20 $ vcat $ replicate 15 $
-           hsep $ map text $ words bigtext)
-
-  , bench "reflow" $
-      nf (render (Just 20) :: Doc -> LazyText)
-         (hsep $ map text $ words . unwords $ replicate 50 bigtext)
-
-  , bench "tabular" $
-      nf (render (Just 80) :: Doc -> LazyText)
-         (let blah = hsep $ map text $ words . unwords
-                           $ replicate 50 bigtext
-          in  cblock 20 blah <> lblock 30 blah <> rblock 10 blah $$
-              cblock 50 (nest 5 blah) <> rblock 10 blah)
-
-  , bench "soft spaces at end of line" $
-      nf (render Nothing :: Doc -> LazyText)
-         ("a" <> mconcat (replicate 50 (space <> box 1 mempty)))
+    bench "reflow" $
+      nf (render (Just 20) :: Doc -> LazyText) flowedDoc
+--
+--  , bench "tabular" $
+--      nf (render (Just 80) :: Doc -> LazyText)
+--         (let blah = hsep $ map text $ words . unwords
+--                           $ replicate 50 bigtext
+--          in  cblock 20 blah <> lblock 30 blah <> rblock 10 blah $$
+--              cblock 50 (nest 5 blah) <> rblock 10 blah)
+--
+--  , bench "soft spaces at end of line" $
+--      nf (render Nothing :: Doc -> LazyText)
+--         ("a" <> mconcat (replicate 50 (space <> box 1 mempty)))
   ]
