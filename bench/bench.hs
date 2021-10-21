b278b5e984a9ca26b9301a2a5e6898f48cec6eac
Stephen Morgan <Xitian9@users.noreply.github.com>
2021-10-22 04:24:55 +1100

    Add benchmarks for all scripts used by more than 50 million people, plus a couple more. (#9)
    Scripts chosen from here:
    https://en.wikipedia.org/wiki/List_of_writing_systems#List_of_writing_systems_by_adoption
    
    Translations taken from here:
    https://www.unicode.org/udhr/translations.html

--- bench/bench.hs
+++ bench/bench.hs
@@ -1,7 +1,13 @@
 {-# LANGUAGE CPP               #-}
 {-# LANGUAGE OverloadedStrings #-}
 import Text.DocLayout
+import Text.Emoji
+
+import Control.DeepSeq (force)
+import Control.Exception (evaluate)
 import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T (readFile)
 import Criterion.Main
 import Criterion.Types (Config (..))
 #if MIN_VERSION_base(4,11,0)
@@ -10,7 +16,47 @@ import Data.Semigroup
 #endif
 
 main :: IO ()
-main = defaultMainWith defaultConfig{ timeLimit = 10.0 } $ cases
+main = do
+    udhrEng <- udhrLang "eng"
+    udhrFrn <- udhrLang "frn"
+    udhrVie <- udhrLang "vie"
+    udhrChn <- udhrLang "chn"
+    udhrArz <- udhrLang "arz"
+    udhrHnd <- udhrLang "hnd"
+    udhrBng <- udhrLang "bng"
+    udhrRus <- udhrLang "rus"
+    udhrJpn <- udhrLang "jpn"
+    udhrKkn <- udhrLang "kkn"
+    udhrTcw <- udhrLang "tcw"
+    udhrTcv <- udhrLang "tcv"
+    udhrThj <- udhrLang "thj"
+    udhrGrk <- udhrLang "grk"
+    emojiTxt <- evaluate . force . T.replicate 1000 $ mconcat baseEmojis <> mconcat zwjEmojis
+    defaultMainWith defaultConfig{ timeLimit = 10.0 } $ cases ++
+      -- Benchmarks for languages using all scripts used by more than 50 million people
+      [ bench "UDHR English"    $ nf realLength udhrEng  -- Plain ASCII
+      , bench "UDHR French"     $ nf realLength udhrFrn  -- Latin with some diacritics
+      , bench "UDHR Vietnamese" $ nf realLength udhrVie  -- Latin with more diacritics
+      , bench "UDHR Mandarin"   $ nf realLength udhrChn  -- Mandarin
+      , bench "UDHR Arabic"     $ nf realLength udhrArz  -- Arabic
+      , bench "UDHR Hindi"      $ nf realLength udhrHnd  -- Hindi
+      , bench "UDHR Bengali"    $ nf realLength udhrBng  -- Bengali
+      , bench "UDHR Russian"    $ nf realLength udhrRus  -- Russian
+      , bench "UDHR Japanese"   $ nf realLength udhrJpn  -- Japanese
+      , bench "UDHR Korean"     $ nf realLength udhrKkn  -- Korean
+      , bench "UDHR Telugu"     $ nf realLength udhrTcw  -- Telugu
+      , bench "UDHR Tamil"      $ nf realLength udhrTcv  -- Tamil
+      -- Benchmarks for other languages
+      , bench "UDHR Thai"       $ nf realLength udhrThj  -- Thai
+      , bench "UDHR Greek"      $ nf realLength udhrGrk  -- Greek
+      , bench "Emoji"           $ nf realLength emojiTxt -- Emoji
+      ]
+
+-- | The Universal declaration of human rights in a given language, repeated 1000 times.
+udhrLang :: String -> IO Text
+udhrLang lang = do
+    txt <- T.readFile ("udhr/txt/" ++ lang ++ ".txt")
+    evaluate . force $ T.replicate 1000 txt
 
 bigtext :: String
 bigtext = "Hello there. This is a big text."
